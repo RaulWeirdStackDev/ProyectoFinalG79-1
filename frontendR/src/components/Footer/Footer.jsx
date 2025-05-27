0a2a374d1ed606d952f6cfaab359973f27c20735
Raúl <raul.rodriguez.c.2012@gmail.com>
2025-05-27 00:34:03 -0400

    frontend Raúl

---
+++ frontendR/src/components/Footer/Footer.jsx
@@ -0,0 +1,21 @@
+import './footer.css'
+
+export const Footer = () => {
+  return (
+    <>
+    <footer>
+    <div id="drixel">
+    <p>DrixelStore 2025.</p>
+    </div>
+    <div id="iconos">
+    <a href="https://www.facebook.com/Drixelstore/" target='_blank'><img id='facebook' src="/facebook.png" alt="" /></a>
+<a href="https://www.instagram.com/drixelstore/" target='_blank'><i className="fa-brands fa-instagram fa-2xl"></i></a>
+
+    </div>
+    <div id="preguntas">
+<p>Preguntas frecuentes</p>
+    </div>
+    </footer>
+    </>
+  )
+}
---
+++ frontendR/src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import './header.css'
+
+export const Header = () => {
+  return (
+    <>
+    <header>
+    <p>Envío gratis a todo Chile por compras sobre $79.990.</p>
+    </header>
+    </>
+  )
+}
+
