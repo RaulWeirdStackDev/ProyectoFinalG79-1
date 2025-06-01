b2bab57e9c8e7cba877e9391be253c8233f8f3da
Darwin Zamarca G <dzamarca@subsole.com>
2025-06-01 14:39:36 -0400

    add - routes tabla rol CRUD -D

--- backend_desafio03/src/controllers/rol.controller.js
+++ backend_desafio03/src/controllers/rol.controller.js
@@ -1,4 +1,4 @@
-import { createRolModel, readRolModel, updateRolModel } from "../models/rol.model.js"
+import { createRolModel, readAllRolModel, readRolActivoModel, updateRolModel } from "../models/rol.model.js"
 // import jwt from 'jsonwebtoken'
 
 export const createRol = async (req, res) => {
@@ -18,7 +18,21 @@ export const readAllRol = async (req, res) => {
         if (!token) {
             return res.status(400).json({ message: 'El token debe estar presente' })
         }
-        const data = await readRolModel()
+        const data = await readAllRolModel()
+        res.status(200).json({ data })
+    } catch (error) {
+        res.status(500).json({ error: 'Error al procesar la solicitud' })
+        console.error('ERROR_CONTROLLER_GET =>', error)
+    }
+}
+
+export const readRolActivo = async (req, res) => {
+    try {
+        const token = req.header('Authorization')
+        if (!token) {
+            return res.status(400).json({ message: 'El token debe estar presente' })
+        }
+        const data = await readRolActivoModel()
         res.status(200).json({ data })
     } catch (error) {
         res.status(500).json({ error: 'Error al procesar la solicitud' })
@@ -28,12 +42,13 @@ export const readAllRol = async (req, res) => {
 
 export const updateRol = async (req, res) => {
     try {
-        const token = req.header('Authorization')
-        if (!token) {
-            return res.status(400).json({ message: 'El token debe estar presente' })
-        }
-        const { descripcion, estado } = req.body
-        const result = await updateRolModel(descripcion, estado)
+        // const token = req.header('Authorization')
+        // if (!token) {
+        //     return res.status(400).json({ message: 'El token debe estar presente' })
+        // }
+        const {id} = req.params
+        const {descripcion, estado } = req.body
+        const result = await updateRolModel(id, descripcion, estado)
         res.status(200).json({ user: result })
     } catch (error) {
         res.status(500).json({ error: 'Error al procesar la solicitud' })
