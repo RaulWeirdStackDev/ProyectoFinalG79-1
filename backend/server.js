591fd294461b6755baed083a559318e31c380e44
EnrikeSekai <eaoo@gmail.com>
2025-06-12 15:38:13 -0400

    Cambiando client por pool y export default pool, todo funciona ok

--- backend/server.js
+++ backend/server.js
@@ -3,19 +3,20 @@ import cors from 'cors'
 import 'dotenv/config'
 
 import userRoutes from './routes/user.routes.js'
-// import authRoutes from './routes/auth.routes.js'
-// import rolRoutes from './routes/rol.routes.js'
-// import categoriaRoutes from './routes/categoria.routes.js'
-// import carouselRoutes from './routes/carousel.routes.js'
-// import regionRoutes from './routes/region.routes.js'
-// import comunaRoutes from './routes/comuna.routes.js'
-// import direccionRoutes from './routes/direccion.routes.js'
-// import productoRoutes from './routes/producto.routes.js'
-// import inventarioRoutes from './routes/inventario.routes.js'
-// import ventaRoutes from './routes/venta.routes.js'
+import authRoutes from './routes/auth.routes.js'
+import rolRoutes from './routes/rol.routes.js'
+import categoriaRoutes from './routes/categoria.routes.js'
+import carouselRoutes from './routes/carousel.routes.js'
+import regionRoutes from './routes/region.routes.js'
+import comunaRoutes from './routes/comuna.routes.js'
+import direccionRoutes from './routes/direccion.routes.js'
+import productoRoutes from './routes/producto.routes.js'
+import inventarioRoutes from './routes/inventario.routes.js'
+import ventaRoutes from './routes/venta.routes.js'
 
 import { log } from './middleware/log.middleware.js'
-import { client } from './db/config.js'
+import pool from './db/config.js'; 
+
 
 const PORT = process.env.PORT || 3000
 const app = express()
@@ -24,22 +25,22 @@ app.use(express.json())
 app.use(cors())
 app.use(log)
 
-// app.use('/api',authRoutes)
-app.use('/api',userRoutes)
-// app.use('/api',rolRoutes)
-// app.use('/api',categoriaRoutes)
-// app.use('/api',carouselRoutes)
-// app.use('/api',regionRoutes)
-// app.use('/api',comunaRoutes)
-// app.use('/api',direccionRoutes)
-// app.use('/api', productoRoutes)
-// app.use('/api', inventarioRoutes)
-// app.use('/api', ventaRoutes)
+app.use('/api', authRoutes)
+app.use('/api', userRoutes)
+app.use('/api',rolRoutes)
+app.use('/api',categoriaRoutes)
+app.use('/api',carouselRoutes)
+app.use('/api',regionRoutes)
+app.use('/api',comunaRoutes)
+app.use('/api',direccionRoutes)
+app.use('/api', productoRoutes)
+app.use('/api', inventarioRoutes)
+app.use('/api', ventaRoutes)
 
 // console.log('Tipo:', typeof process.env.DB_PASSWORD, 'Valor:', process.env.DB_PASSWORD)
 
 app.listen(PORT, console.log(`🍒 Server http://localhost:${PORT}`))
-client.connect()
+pool.connect()
     .then(() => console.log("✅ Conectado correctamente"))
     .catch(err => console.error("❌ Error conectando a DB:", err));
 
