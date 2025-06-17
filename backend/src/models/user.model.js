e87ad074080cfc3a81d42f73538230005fd59531
EnrikeSekai <eaoo@gmail.com>
2025-06-16 23:26:54 -0400

    Register con rut y tlf

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,47 +1,51 @@
 import pool from '../../db/config.js'
 import bcrypt from 'bcryptjs'
 
-export const createUserModel = async (nombre, apellido, email, password) => {
+export const createUserModel = async (nombre, apellido, email, password, rut, telefono) => {
   try {
     const hashedPassword = bcrypt.hashSync(password);
     const sqlQuery = {
-      text: 'INSERT INTO usuario (nombre, apellido, email, password) VALUES ($1, $2, $3, $4) RETURNING nombre, apellido, email',
-      values: [nombre, apellido, email, hashedPassword]
+      text: `
+        INSERT INTO usuario (nombre, apellido, email, password, rut, telefono)
+        VALUES ($1, $2, $3, $4, $5, $6)
+        RETURNING nombre, apellido, email, rut, telefono
+      `,
+      values: [nombre, apellido, email, hashedPassword, rut, telefono]
     };
     const response = await pool.query(sqlQuery);
     return response.rows[0];
   } catch (error) {
     console.error('Error en createUserModel:', error);
-    throw error; 
+    throw error;
   }
-}
+};
 
-export const findUserByEmailModel = async(email) => {
-    const sqlQuery = {
-        text: `
+export const findUserByEmailModel = async (email) => {
+  const sqlQuery = {
+    text: `
           SELECT u.*, r.id_rol, r.descripcion AS rol_descripcion
           FROM usuario u
           LEFT JOIN rol r ON u.id_rol = r.id_rol
           WHERE u.email = $1
         `,
-        values: [email]
-    }
-    const response = await pool.query(sqlQuery)
-    return response.rows[0]
+    values: [email]
+  }
+  const response = await pool.query(sqlQuery)
+  return response.rows[0]
 }
 
-export const findUserByIdModel = async(id_usuario) => {
-    const sqlQuery = {
-        text: `
+export const findUserByIdModel = async (id_usuario) => {
+  const sqlQuery = {
+    text: `
           SELECT u.*, r.id_rol, r.descripcion AS rol_descripcion
           FROM usuario u
           LEFT JOIN rol r ON u.id_rol = r.id_rol
           WHERE u.id_usuario = $1
         `,
-        values: [id_usuario]
-    }
-    const response = await pool.query(sqlQuery)
-    return response.rows[0]
+    values: [id_usuario]
+  }
+  const response = await pool.query(sqlQuery)
+  return response.rows[0]
 }
 
 export const updateUserModel = async (id, nombre, apellido, password, rut, telefono, img) => {
